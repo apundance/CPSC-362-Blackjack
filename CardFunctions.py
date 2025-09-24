3104317282b9d00d9a4a28e8a38c23c4bb7efe63
apundance <enoch.liu@csu.fullerton.edu>
2025-09-23 21:19:23 -0700

    Added the deal cards function

--- CardFunctions.py
+++ CardFunctions.py
@@ -1,9 +1,13 @@
+import random
 
- # Deck of cards
+# Deck of cards
 
 Deck = [2, 3, 4, 5, 6, 7, 8, 9, 'J', 'Q', 'K', 'A']
 playerHand = []
 dealrHand = []
 
 # Deal hands
-
+def DealCard(Turn):
+    card = random.choice(Deck)
+    Turn.append(card)     
+    Deck.remove(card)
