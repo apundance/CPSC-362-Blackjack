a5d5bb2916c21d2dc61fc8d9446205dc8a998b2a
tricar534 <tricar534@gmail.com>
2025-10-01 10:14:18 -0700

    Print total and added jpegs for diagrams
    When running main blackjack fil you can now see output of player and dealer as well as their totals. Included jpegs of diagrams we worked on.

--- Blackjack.py
+++ Blackjack.py
@@ -2,10 +2,14 @@ import CardFunctions
 
 playerHand = []
 dealerHand = []
+
 # Main game loop
 for _ in range(2):
     CardFunctions.DealCard(playerHand)
     CardFunctions.DealCard(dealerHand)
 
-print(f"Player Hand = {playerHand} Dealer Hand = {dealerHand}")
+totalPlayer = CardFunctions.Total(playerHand)
+totalDealer = CardFunctions.Total(dealerHand)
+
+print(f"Player Hand = {playerHand} player total is: {totalPlayer}\nDealer Hand = {dealerHand}, dealer total is: {totalDealer}" )
 
