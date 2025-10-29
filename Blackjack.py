863f66206ba4aba3ef8e942b855b9d5b8d09fe31
tricar534 <tricar534@gmail.com>
2025-10-29 09:04:34 -0700

    Update Blackjack.py
    New window with name of game and exit button to close window

--- Blackjack.py
+++ Blackjack.py
@@ -10,3 +10,21 @@ width, height = 1000, 700
 screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 pygame.display.set_caption("BlackjackTesting")
 clock = pygame.time.Clock()
+
+running = True
+
+# main loop
+# ───────────────────────────────
+while running:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            running = False
+
+
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()
+sys.exit()
